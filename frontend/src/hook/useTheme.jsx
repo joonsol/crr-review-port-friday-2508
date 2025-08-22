c91680236d309696eee8063fe550a51ba5dfae06
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-22 21:40:55 +0900

    update

--- frontend/src/hook/useTheme.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const useTheme = () => {
-  return (
-    <div>useTheme</div>
-  )
-}
-
-export default useTheme
--- frontend/src/pages/AdminPost.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminPost = () => {
-  return (
-    <div>AdminPost</div>
-  )
-}
-
-export default AdminPost
