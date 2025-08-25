aa85b41c64fb4bba55ae42d4035cce5286002e6a
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-25 22:13:09 +0900

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import './App.scss'
 import Notfound from './pages/Notfound'
 import Home from './pages/Home'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import AdminLogin from './pages/AdminLogin'
 import AdminPost from './pages/AdminPost'
 import AuthRedirectRoute from './routes/AuthRedirectRoute';
@@ -17,14 +17,14 @@ function App() {
   return (
     <ThemeProvider>
 
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      {/* 로그인 페이지: 이미 인증이면 /admin/posts로 우회 */}
-      <Route path="/admin" element={<AuthRedirectRoute Component={AdminLogin} />} />
-      {/* 보호 페이지: 비인증이면 /admin/login으로 */}
-      <Route path="/admin/posts" element={<RequireAuth Component={AdminPost} />} />
-      <Route path='*' element={<Notfound/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* 로그인 페이지: 이미 인증이면 /admin/posts로 우회 */}
+        <Route path="/admin" element={<AuthRedirectRoute Component={AdminLogin} />} />
+        {/* 보호 페이지: 비인증이면 /admin/login으로 */}
+        <Route path="/admin/posts" element={<RequireAuth Component={AdminPost} />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
     </ThemeProvider>
   )
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,26 +8,26 @@ const Hero = () => {
       <h1 className="tit">
         <span>
           developer
-          </span>
-          <span>
-         
-            back <span className="star-spin"><i className="star">✱</i></span> front
-          </span>
-          <span>
-            portfolio
-          </span>
+        </span>
+        <span>
+
+          back <span className="star-spin"><i className="star">✱</i></span> front
+        </span>
+        <span>
+          portfolio
+        </span>
       </h1>
       <p className="txt">
-        시맨틱 마크업을 통해 웹 접근성, 
-        <br/>
+        시맨틱 마크업을 통해 웹 접근성,
+        <br />
         웹 표준을 준수하여 차별이 없는 웹을 지향합니다.
-        <br/>
+        <br />
         node express -   react를  활용한 MERN 프로젝트를 지향하며,
-        <br/>
+        <br />
         풀스택 전문가로써 성장과 도전을 지향합니다.
-        <br/>
+        <br />
       </p>
-      </div>
+    </div>
   )
 }
 
