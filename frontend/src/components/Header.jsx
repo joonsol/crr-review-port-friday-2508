0c3cd7f77bf4c2c2ee5048edf1c38d26011a4e9e
joonsol <71648638+joonsol@users.noreply.github.com>
2025-08-25 22:12:26 +0900

    hero

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,12 +2,27 @@ import React from 'react'
 import Nav from './Nav'
 import "./styles/Header.scss"
 import { ThemeProvider, useTheme } from '../contexts/ThemeContext'
+import { useState,useEffect } from 'react'
 
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
+  const [scrolled, setScrolled]=useState(false)
+
+  useEffect(()=>{
+
+    const handleScroll =()=>{
+      if(window.scrollY>50){
+        setScrolled(true)
+      }else{
+        setScrolled(false)
+      }
+    }
+    window.addEventListener('scroll',handleScroll)
+  },[])
 
   return (
-    <header className='Header'>
+
+    <header className={`Header ${scrolled? "scroll":""}`}>
       <div className="inner">
 
         <h4>LOGO</h4>
